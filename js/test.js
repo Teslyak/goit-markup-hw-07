0374dbc0316ab9f413a37ad450b6f3b372728491
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-05-08 15:40:28 +0300

    080523

--- js/test.js
+++ js/test.js
@@ -86,17 +86,162 @@
 // formatMessage("Nunc sed turpis a felis in nunc fringilla", 15);
 // formatMessage("Nunc sed turpis a felis in nunc fringilla", 41);
 
-let login = prompt("Enter you login");
-let password;
-if (!login) {
-  console.log ("Скасовано");
-} else if (login === "Адмін") {
-  password = prompt("Enter you password");
-  if (password === "Я адмін") {
-    console.log("Здрастуйте!");
-  } else {
-    console.log("Невірний пароль");
-  }
-} else {
-  console.log("Я вас не знаю");
+// let login = prompt("Enter you login");
+// let password;
+// if (!login) {
+//   console.log ("Скасовано");
+// } else if (login === "Адмін") {
+//   password = prompt("Enter you password");
+//   if (password === "Я адмін") {
+//     console.log("Здрастуйте!");
+//   } else {
+//     console.log("Невірний пароль");
+//   }
+// } else {
+//   console.log("Я вас не знаю");
+// }
+
+// function getExtremeElements(array) {
+//   // Change code below this line
+//   const firstElement = array[0];
+//   console.log(firstElement);
+//   const endElement = array[array.length - 1];
+//   console.log(endElement);
+//   const array2 = [firstElement , endElement];
+//   console.table(array2);
+// return array2  ;
+//   // Change code above this line
+// }
+
+// getExtremeElements([1, 2, 3, 4, 5]);
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//    // Change code below this line
+//   message = message.split(" ").length;
+//   const totalPrice = message * pricePerWord;
+// console.log(totalPrice);
+// return totalPrice
+
+//    // Change code above this line
+// }
+
+// calculateEngravingPrice("JavaScript is in my blood", 10);
+// calculateEngravingPrice("JavaScript is in my blood", 20);
+
+// function slugify(title) {
+//   // Change code below this line
+//   let slug = " ";
+//   slug = title.toLowerCase().split(" ").join("-");
+  
+// return slug;
+
+//   // Change code above this line
+// }
+
+// slugify("Arrays for begginers");
+
+
+
+const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// // Change code below this line
+// const firstTwoEls = fruits.slice(0 , 2) ;
+// const nonExtremeEls = fruits.slice(1, 5) ;
+// const lastThreeEls = fruits.slice(-3);
+// console.log(firstTwoEls);
+// console.log(nonExtremeEls);
+// console.log(lastThreeEls);
+ 
+// const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// const newClients = ['Peach', 'Houston'];
+
+// const allClients = oldClients.concat(newClients);
+// console.log(allClients);
+// // Change this line
+
+// function makeArray(firstArray, secondArray, maxLength) {
+//   // Change code below this line
+//   let newArray = firstArray.concat(secondArray);
+//   newArray = newArray.length > maxLength ? newArray.slice(0, maxLength) : newArray.slice(0, newArray.length);
+// console.log(newArray);
+// }
+
+// makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3);
+// makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4);
+
+// function calculateTotal(number) {
+//  // Change code below this line
+//   let totalSumNumber = 0;
+//   for (i = 1; i <= number; i += 1) {
+//     totalSumNumber += i;
+    
+//   }
+//  console.log(totalSumNumber);
+// }
+// calculateTotal(10);
+
+
+//   // Change code above this line
+
+// function findLongestWord(string) {
+//   let arrayWords = string.split(" ");
+//   let bigWord = arrayWords[0];
+//   for (i = 1; i < arrayWords.length; i +=1 ) {
+    
+//     if (bigWord.length < arrayWords[i].length) {
+//       bigWord = arrayWords[i];
+//     }
+    
+//   }
+//   return bigWord;
+// }
+// findLongestWord("The quick brown fox jumped over the lazy dog");
+
+
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   // Change code below this line
+//   for (i = min; i <= max; i += 1) {
+//     numbers.push(i);
+//   }
+//   console.log(numbers);
+//   return numbers;
+  
+// }
+
+//   // Change code above this line
+
+// createArrayOfNumbers(29, 34);
+
+// createArrayOfNumbers(14, 17);
+
+function filterArray(numbers, value) {
+   // Change code below this line
+  let newArray = [];
+  for (const number of numbers) {
+    // console.log(number);
+    if (number > value) {
+      newArray.push(number);
+    }
+}
+
+console.log(newArray);
+  // Change code above this line
+}
+filterArray([1, 2, 3, 4, 5], 3);
+filterArray([1, 2, 3, 4, 5], 4);
+filterArray([1, 2, 3, 4, 5], 5);
+filterArray([12, 24, 8, 41, 76], 38);
+filterArray([12, 24, 8, 41, 76], 20);
+
+function getExtremeElements(array) {
+  // Change code below this line
+  const firstElement = array[0];
+  console.log(firstElement);
+  const endElement = array[array.length - 1];
+  console.log(endElement);
+  const array2 = [firstElement , endElement];
+  console.table(array2);
+return array2  ;
+  // Change code above this line
 }
