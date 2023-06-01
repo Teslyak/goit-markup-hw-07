219576c5c70a9fe50967edefb01e595f1ffd3c23
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-01 16:42:23 +0300

    010623

--- js/test.js
+++ js/test.js
@@ -1288,24 +1288,33 @@
 // console.log(builder.getValue()); // "=^.^="
 
 
-class Car {
-  static #MAX_PRICE = 50000;
-  // Change code below this line
- static checkPrice (price) {
-   if (price >= Car.#MAX_PRICE) {
-    return "Error! Price exceeds the maximum";    
-   } 
-     return "Success! Price is within acceptable limits";
- } 
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Change code below this line
+//  static checkPrice (price) {
+//    if (price >= Car.#MAX_PRICE) {
+//     return "Error! Price exceeds the maximum";
+//    }
+//      return "Success! Price is within acceptable limits";
+//  }
   
-  // Change code above this line
-  constructor({ price }) {
-    this.price = price;
-  }
+//   // Change code above this line
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+const person = { name: 'Mango' };
+
+function sayHi(age) {
+  return `${this.name} is ${age}`;
 }
 
-const audi = new Car({ price: 36000 });
-const bmw = new Car({ price: 64000 });
-
-console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
-console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+console.log(sayHi.call(person, 18));
+console.log(sayHi.bind(person, 18)); 
